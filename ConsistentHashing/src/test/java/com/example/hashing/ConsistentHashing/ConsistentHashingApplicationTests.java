d0379a4d724d9a2f8cf5cb50dc69391cc3800d7e
ManojSK462 <saimanojkonidana462@gmail.com>
2025-01-19 02:18:57 -0600

    First Commit : Building Consistent Hashing(Hash Ring) for Distributed Cache Servers

---
+++ ConsistentHashing/src/test/java/com/example/hashing/ConsistentHashing/ConsistentHashingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hashing.ConsistentHashing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsistentHashingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
